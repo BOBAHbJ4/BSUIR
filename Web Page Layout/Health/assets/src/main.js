d37d08234b1c17e0ccf58c38d6c25e25e5aea338
Владимир <spysystems.87@gmail.com>
2022-01-19 20:35:02 +0300

    add js
    add modal window to recepies.html

--- Web Page Layout/Health/assets/src/main.js
+++ Web Page Layout/Health/assets/src/main.js
@@ -1,1 +1,27 @@
-alert ('Javascript подключён');
+$(function () {
+    var fx = {
+        "initModal": function () {
+            if ($(".modal-window").length == 0) {
+                $("<div>").addClass("overlay").appendTo("body");
+                return $("<div>").addClass("modal-window").appendTo("body");
+            } else {
+                return $(".modal-window");
+            }
+        }
+    }
+    $(".slider-item__btn").click(function () {
+        var id = $(this).attr("data-pic-id");
+        var modal = fx.initModal();
+        $("<a>").attr("href", "#").addClass("modal-close").html("&times;").click(function (event) {
+            event.preventDefault();
+            modal.remove();
+            $(".overlay").remove();
+        }).appendTo(modal);
+        var item=$("#item-"+id).text();
+        var pic=$("#"+id).clone();
+        var content=$(this).attr("data-content");
+        modal.append(item);
+        modal.append(pic);
+        modal.append(content);
+    });
+});
