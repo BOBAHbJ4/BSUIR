fd51c7c71cccd8add71f0eb3d293b13ba323ff7f
Владимир <spysystems.87@gmail.com>
2023-01-18 23:27:48 +0300

    del old proj
    del old proj

--- Web Page Layout/Health/assets/src/script.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector('.hamburger-menu');
-const navBar = document.querySelector('.nav-wrapper');
-
-menuIcon.addEventListener('click', () => {
-    navBar.classList.toggle('active');
-});
